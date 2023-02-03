bcdf4300a16860681d75df8d3d24f222aa7bc729
dejourford <dejourford@my.unt.edu>
2023-02-03 13:10:07 -0600

    added icons and spacing to header

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-const navigation = document.querySelector('.navigation')
-const links = document.querySelector('.links')
-navigation.addEventListener('click', function(){
-    links.style.display = 'inline-flex'
-})
+
