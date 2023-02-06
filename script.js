6f87fcb5420224362fc5bed805a5dd2cfe0ad246
De'Jour Ford <dejourford@my.unt.edu>
2023-02-06 01:50:16 -0600

    create spacers to help with aesthetic

--- script.js
+++ script.js
@@ -70,3 +70,23 @@ typewriter = setupTypewriter(typewriter);
 
 typewriter.type();
 
+// Scroll back to top
+// Get the button:
+let mybutton = document.getElementById("top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
